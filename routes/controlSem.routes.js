b3635fc744b9e9b9fc06959304409eb5d43254b5
Waly666 <walter_a_aguilar@hotmail.com>
2026-03-29 01:16:46 -0500

    feat: ARREGLO DE FORMULARIOS BACKEND

--- routes/controlSem.routes.js
+++ routes/controlSem.routes.js
@@ -5,8 +5,9 @@ const { authenticateToken, hasRole } = require('../middlewares/auth.middleware')
 const { checkJornada } = require('../middlewares/jornada.middleware');
 
 router.get('/',       authenticateToken,                                              controller.getAll);
-router.get('/:id',    authenticateToken,                                              controller.getById);
+/* Debe ir antes de /:id para que "tramo" no se interprete como id */
 router.get('/tramo/:idViaTramo', authenticateToken, controller.getByTramo);
+router.get('/:id',    authenticateToken,                                              controller.getById);
 router.post('/',      authenticateToken, hasRole('admin','supervisor','encuestador'), checkJornada, controller.create);
 router.put('/:id',    authenticateToken, hasRole('admin','supervisor','encuestador'), checkJornada, controller.update);
 router.delete('/:id', authenticateToken, hasRole('admin','supervisor'),               controller.remove);
